46547c10c680a32629d95c32738bf0a971dca747
dev@nettools.ovh <dev@nettools.ovh>
2021-06-12 10:28:48 +0200

    escpos filters always return raw text

--- src/EscPosTemplateCreator.php
+++ src/EscPosTemplateCreator.php
@@ -49,7 +49,7 @@ class EscPosTemplateCreator extends TemplateCreator {
 		$twig->addFilter(
 				new \Twig\TwigFilter('html2escpos', function ($string) {
 					  return str_replace(['<b>', '<strong>', '</b>', '</strong>', '<u>', '</u>'], [chr(27).'E1', chr(27).'E1', chr(27).'E0', chr(27).'E0', chr(27).'-1', chr(27).'-0'], strip_tags($string, '<b><strong><u>'));
-				})
+				}, ['is_safe' => ['all']])
 			);
 		
 		
@@ -57,42 +57,42 @@ class EscPosTemplateCreator extends TemplateCreator {
 		$twig->addFilter(
 				new \Twig\TwigFilter('bold', function ($string) {
 					  return chr(27).'E1' . $string . chr(27).'E0';
-				})
+				}, ['is_safe' => ['all']])
 			);
 
 		// register filter to output UNDERLINED text
 		$twig->addFilter(
 				new \Twig\TwigFilter('underline', function ($string) {
 					  return chr(27).'-1' . $string . chr(27).'-0';
-				})
+				}, ['is_safe' => ['all']])
 			);
 		
 		// register filter to output HEAVY UNDERLINED text
 		$twig->addFilter(
 				new \Twig\TwigFilter('underlineheavy', function ($string) {
 					  return chr(27).'-2' . $string . chr(27).'-0';
-				})
+				}, ['is_safe' => ['all']])
 			);
 		
 		// register filter to output CENTERED text
 		$twig->addFilter(
 				new \Twig\TwigFilter('center', function ($string) {
 					  return chr(27).'a1' . $string . "\n" . chr(27).'a0';
-				})
+				}, ['is_safe' => ['all']])
 			);
 		
 		// register filter to output RIGHT-ALIGNED text
 		$twig->addFilter(
 				new \Twig\TwigFilter('right', function ($string) {
 					  return chr(27).'a2' . $string . "\n" . chr(27).'a0';
-				})
+				}, ['is_safe' => ['all']])
 			);
 		
 		// register filter to output FONTB text (reverting to FONTA at the end of output)
 		$twig->addFilter(
 				new \Twig\TwigFilter('fontB', function ($string) {
 					  return chr(27).'M1' . $string . "\n" . chr(27).'M0';
-				})
+				}, ['is_safe' => ['all']])
 			);
 	}
     
